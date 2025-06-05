16cb0eabe9a1f22a7ff2a8a3f717326a35e971d5
Abdullah <mdaaf.abdullah@gmail.com>
2025-06-05 14:57:02 +0000

    conersion operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,24 +1,75 @@
 
 let score= "Abdullah ibahim" //we know this is string
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(valueInNumber);//this is not a number like NaN.what the fuck is that.
-console.log(typeof valueInNumber); //this is actually number.
-
+// console.log(valueInNumber); //this is not a number like NaN.what the fuck is that.
+// console.log(typeof valueInNumber); //this is actually number.
 
 let aValue1 = 69 
 let aValue2 = "69abc"
 let scoree;
-console.log(typeof aValue2);
-console.log(typeof aValue1);
-console.log(typeof scoree);
+// console.log(typeof aValue1);
+// console.log(typeof aValue2);
+// console.log(typeof scoree);
 
-/* "45" => 45
+/*
+ "45" => 45
 "67xyz" => NaN
 true => 1 ; false => 0
 */
+
+let loggedIn = ""
+let isHeLoggedIn;
+let isLoggedIn = "ashiyan"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+let isBooleanisLoggedin = Boolean(isHeLoggedIn)
+let booleanloggedIn = Boolean(loggedIn)
+
+// console.log(booleanIsLoggedIn); //is boolean is true.
+// console.log(isBooleanisLoggedin);
+// console.log(booleanloggedIn);
+
+
+/*
+1 => true ; 0=> false.
+"" or '' => false
+ ashiyan => true
+*/
+
+let aNumber = 26
+let stringNumber = String(aNumber)
+
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+//if something we need to string value we convered the value like this string().
+
+///====================OPERATIONS===============================
+
+let value1 = 2
+let negValue = - value1
+// console.log(negValue);// -2
+// console.log(2+2); //4
+// console.log(2-2); //0
+// console.log(2 * 7); //14
+// console.log(2 ** 3); //this is like a power 2 tp the power 3.
+// console.log(4/5); //
+// console.log(100%22);//12
+
+let str1 = 'hey '
+let str2 = "abdulah"
+let str3 = str1 + str2
+console.log(str3); //hey abdullah
+
+console.log("3" + 2); //32
+console.log(2 + "3"); //23
+console.log('2' + 2); //2
+console.log("3" + 2 + 3); //323
+console.log(4 + 2 + "3"); //43
+console.log( (3 + 4) * 5 % 3); //2
+ console.log(+true);//1
+console.log(+""); //0
 
 
 // node 01_basics/03_conversionOperation.js
