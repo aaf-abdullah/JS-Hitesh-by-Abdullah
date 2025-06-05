650732427feb103ddb88e3c8e96d218fce844905
Abdullah <mdaaf.abdullah@gmail.com>
2025-06-05 00:08:36 +0000

    VARIABLES

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,11 +1,56 @@
-const accountId = 1323
-let accountEmail = "abdullah@gmail.accountEmail"
-var acountPassword = "12345"
-acountCity = "chittagong"
-
-accountId = 2
-
-console.log(accountId);
+const accountId = 233026
+let accountEmail = "abdullah@gmail.com"
+var accountPassword = "12345567"
+accountCity = "chittagong"
+let accountState;
+//  accountId = 3254523
 
 
+accountEmail = 'abd@fahim.com'
+accountPassword = "36543643"
+accountCity = 'bakolia ctg'
+
+// console.log(accountId);
+// console.table([accountId, accountState,accountEmail, accountCity, accountPassword])
+
+// in console.table there output is basially two row and these much inpu column what gives inside the input.
+
+
+//this is not allowed in js becouse account id is constant variable,this variale hold a value with his declaration.
+
+// prefer not use var becuse of issue in block scope and functional scope.what is block and functional scope knowing the next..... 
+
+
+// REAL WORLD EXAMPLE
+
+const myName = 'Ibrahim Abdulllah'
+const myInstitute = "iiuc"
+const myId = 'ET233026'
+let my1stSemeterCG = 3.001
+let myCurrentState = "chittagong bakolia"
+let myEmail = 'abc@gmail.com'
+let myPassword = "abd347df"
+myDepartment = "Electrical and Electronics Engineering"
+let myCourseTeacher;
+
+// so i write const and let.becouse const is use where i dont change my value like my name,my institute,my id, and let is used in cg,current state,mail,password, this acually mean that const is used in fixed value in the another let is used in chnagble value.
+
+console.table([myName, myInstitute, myId, my1stSemeterCG, myCurrentState, myEmail, myPassword, myCourseTeacher, myDepartment]);
+
+// myInstitute = "MIT, harvard, Oxford" 
+//behenxd aukat dek teri.this is constant varaiable.
+
+myCurrentState = 'GEC'
+myEmail = "abd.@gamil.com"
+myCourseTeacher = 'hitesh chowdhury'
+myDepartment = 'computer science and Engineering'
+
+
+console.table([myName, myInstitute, myId, my1stSemeterCG, myCurrentState, myEmail, myPassword, myCourseTeacher, myDepartment]);
+
+
+
+
+// +++++++++++++++++++++++topic-2+++++++++++++++
+// data types
 
