1d54ad6d3c0158584eafc73974dca2464be0960e
Abdullah <mdaaf.abdullah@gmail.com>
2025-06-05 08:21:22 +0000

    datatypes for javascript

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,47 @@
+
+
+
+
+// "use srict"; //treat al l js code as newer version.
+
+// alert(3+3) //we are using nodejs,not browser.
+
+// console.log(3 + 3); //code readability sould be high.
+// console.log("abdullah");
+
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = true
+let state;
+
+console.log();      //just print a space
+console.log(typeof isLoggedIn); //boolean
+console.log(typeof undefined);  //undefined
+console.log(typeof null);   //object
+console.log(typeof name);   //string
+console.log(typeof age);    //number
+console.log(typeof state);  //undefined
+console.table([typeof null,typeof undefined, typeof isLoggedIn,typeof age,typeof state,typeof name])
+
+
+/*  
+
+so i tried to reveal the typeof of every variables value.like name value,age value type,islooged in type, or null or undefined type of every value has their own type of like name has string types ,age has number tyoes ,null has object tyoe,or true false has boolean type of.
+
+    number => 2 to the power 53 .
+    bigint
+    string => "" or ''
+    boolean => true or false
+    null => standalone value
+    undefined =>  
+    symbol => unique
+    object => null, or .......
+*/
+
+// node 01_basics/02_dataTypes.js //for run
+// FOR MY PRACTISE
+
+
+
+
