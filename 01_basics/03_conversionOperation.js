1e640e7c80e90d9d33e1592c99f9351855a8488d
Abdullah <mdaaf.abdullah@gmail.com>
2025-06-05 13:33:18 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+
+let score= "Abdullah ibahim" //we know this is string
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);//this is not a number like NaN.what the fuck is that.
+console.log(typeof valueInNumber); //this is actually number.
+
+
+let aValue1 = 69 
+let aValue2 = "69abc"
+let scoree;
+console.log(typeof aValue2);
+console.log(typeof aValue1);
+console.log(typeof scoree);
+
+/* "45" => 45
+"67xyz" => NaN
+true => 1 ; false => 0
+*/
+
+
+// node 01_basics/03_conversionOperation.js
+
