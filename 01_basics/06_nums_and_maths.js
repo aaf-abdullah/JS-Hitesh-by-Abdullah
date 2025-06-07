aebbf42fc0ab0619ac7299d34e9652734b9fc6b9
Abdullah <mdaaf.abdullah@gmail.com>
2025-06-07 15:46:11 +0000

    lets learn about nums and maths

--- 01_basics/06_nums_and_maths.js
+++ 01_basics/06_nums_and_maths.js
@@ -1,8 +1,29 @@
+const score = 300
+// console.log(score); //this is we dont defiend num or not.
+
+const balance =new Number(2500)// but there is specificly defined this is number data types.
+
+// console.log(balance);// [Number: 200]
+
+//  console.log(balance.toString().length);// so this basically defined how long the number is in string.
+//  console.log(balance.toFixed(5))// in this keywords they defined how many 0 affter the number.
+
+const fractionNumber = 425.34332
+// console.log(fractionNumber.toPrecision(7)); // this keyword deffined that how many number count depend on the given conditon.
+
+const hundreds = 2000000
+// console.log(hundreds.toLocaleString('en-BD'));//2,000,000
+// console.log(hundreds.toLocaleString('en-IN'));//20,00,000
+// this is defined a count of bd or in s culture.
+
+
+
+//===============MATHS=====================
+
+console.log(Math); //Object [Math] {}
 
 
 
 
 
-
-
-//  node 01_basics/06_nums_and_maths.js
+// node 01_basics/06_nums_and_maths.js
