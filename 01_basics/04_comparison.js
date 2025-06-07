3ac1c77e3aed3d9a158643581e56092cd7eff431
Abdullah <mdaaf.abdullah@gmail.com>
2025-06-07 00:23:43 +0000

    comparison

--- 01_basics/04_comparison.js
+++ 01_basics/04_comparison.js
@@ -1,7 +1,17 @@
 
 
-// console.log(2 > 1);
-// console.log(2 >= 1);
-// console.log(2 < 1);
-// console.log(2 == 1);
-// console.log(2 != 1);
+//  console.log(2 > 1); //true
+// console.log(2 >= 1); //true
+// console.log(2 < 1); //false
+// console.log(2 == 1); //false
+// console.log(2 != 1); //true
+
+console.log('8' < 3); //false 
+console.log("05" > 4); //true
+
+
+
+
+
+
+// node 01_basics/04_comparison.js
