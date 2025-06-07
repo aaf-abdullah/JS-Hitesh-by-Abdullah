b78d9cc38dcd049d006abfb3493c89c850355a50
Abdullah <mdaaf.abdullah@gmail.com>
2025-06-07 11:52:38 +0000

    knowing about strings

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -14,7 +14,7 @@ let name = "hitesh"
 let age = 18
 let isLoggedIn = true
 let state;
-
+/* 
 console.log();      //just print a space
 console.log(typeof isLoggedIn); //boolean
 console.log(typeof undefined);  //undefined
@@ -22,8 +22,9 @@ console.log(typeof null);   //object
 console.log(typeof name);   //string
 console.log(typeof age);    //number
 console.log(typeof state);  //undefined
-console.table([typeof null,typeof undefined, typeof isLoggedIn,typeof age,typeof state,typeof name])
 
+console.table([typeof null,typeof undefined, typeof isLoggedIn,typeof age,typeof state,typeof name])
+*/
 
 /*  
 
@@ -38,10 +39,35 @@ so i tried to reveal the typeof of every variables value.like name value,age val
     symbol => unique
     object => null, or .......
 */
+  
 
-// node 01_basics/02_dataTypes.js //for run
-// FOR MY PRACTISE
+/*  
 
+node 01_basics/02_dataTypes.js
 
+for run
+FOR MY PRACTISE
+DATA TYPES SYMMERY 
+there is two types of data types
 
+1.premitive
+(string
+nummber
+boolean
+null undefined
+symbol
+bigInt)
+
+2.non-premitive/reference types
+(array
+obejects
+function
+)
+
+STACK AND HEAP MEMORY
+
+MEMORY IS TWO TYPES
+1.STACK => PREMITIVE => THIS IS ONE TYPES OF COPY
+2. HEAP => NON-PREMITIVE => THIS IS A REFERENCCE TYPES
+[This is a theory topic if better knowing on thhis then go to the documentation]
 
--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -1,1 +1,26 @@
 //string is actually this ( '' or "")
+//there is two types of string declaration.
+
+const name = 'adil rahman ' //string declaration 1.
+const repositoryCount = 44
+//console.log(typeof name)
+
+// console.log(name + repositoryCount + "value");
+//this types of written is outdated,you write it this types of code bit this is not a readable code
+
+// console.log(`Hello my name is ${name} and my reposatory count is ${repositoryCount}`); 
+//so this the modorn way to write code
+
+const gameName = new String('adil rahman') //string declaration 2. 
+// and one thing should notice on this declaration string is s should be capital S.
+console.log(gameName[0]) // to access the 0th position,1th or 2th or many more have on this string
+console.log(gameName.__proto__); // to object
+console.log(gameName.length); //how many long this word 
+console.log(gameName.toUpperCase()) //to small letter to capital letter
+console.log(gameName.charAt(7))
+console.log(gameName.indexOf("t"))
+console.log(gameName.indexOf("r"))
+
+
+
+//node 01_basics/05_strings.js
