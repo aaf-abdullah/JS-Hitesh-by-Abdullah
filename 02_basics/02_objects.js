78dc147a2bd832da235f2988e55bb9383e23bfc0
Abdullah <mdaaf.abdullah@gmail.com>
2025-06-09 10:45:11 +0000

    deep down into the objects.

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -41,12 +41,52 @@ JsUser.greetingTwo = function(){
 // console.log(JsUser.greeting); //[Function (anonymous)] when freeze is comment then this ans shown
 // console.log(JsUser.greetingTwo);
 
-console.log(JsUser.greeting()); // hello is user motherfather
-console.log(JsUser.greetingTwo()); //hello JS User, faasle
+// console.log(JsUser.greeting()); // hello is user motherfather
+// console.log(JsUser.greetingTwo()); //hello JS User, faasle
 
 
 
 //================PART-2======================
+
+//how to declare a object in constructor singleton
+
+//const tinderUser = new Object() // this is a singleton object
+const tinderUser = {} //non single ton object
+
+tinderUser.id = "124abx"
+tinderUser.name = 'binod'
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+
+const regulerUser = {
+    email: 'binod@google.com',
+    full_name: {
+        userfullname: {
+            firstname: 'binod',
+            lastname: 'bruhh'
+        }
+    }
+}
+
+//console.log(regulerUser.full_name.userfullname.lastname);
+
+
+const obj1 = {1: 'a', 2: 'b'}
+const obj2 = {3: 'c', 4: 'd'}
+const obj3 = {5: 'e', 6: 'f'}
+
+const obj4 = { obj1,  obj2} //this is doing object inside thhe object
+//console.log(obj4);
+
+const obj5 = Object.assign ({}, obj1, obj2, obj3)//to marged on one object 
+//console.log(obj5);
+
+const obj6 = {...obj1, ...obj2, ...obj3} //this doing same things what assign can dom marge in one obj.
+//console.log(obj6);
+
+
+
 
 
 
