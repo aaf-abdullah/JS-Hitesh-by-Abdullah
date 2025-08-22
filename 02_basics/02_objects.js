b11e22a8b86139413efcf236c4a25c93606749fb
Abdullah <mdaaf.abdullah@gmail.com>
2025-08-22 20:11:14 +0000

    deep down in object.

--- 02_basics/02_objects.js
+++ 02_basics/02_objects.js
@@ -2,6 +2,9 @@
 
 //we talking about object literals
 
+
+//HOW TO DECLARE OBJECT LITERALS.
+
 const mysym = Symbol('key1')
 const JsUser = {
     name: 'faasle',
@@ -14,10 +17,22 @@ const JsUser = {
     lastLoginDays: ['Satruday', 'Friday']
 }
 
+
+
+
+// HOW T ACCES THE OBJECT
+
 // console.log(JsUser.email);// this type of access should avoid
 // console.log(JsUser['email']); //instead of this
 // console.log(JsUser['full name']);
 // console.log(JsUser[mysym]);// undefined !! and this keyword access only this method there is none other than/
+
+
+
+
+
+
+//HOW TO ADD NEW KEYS AND HOW TO REPLACE THE VALUES,AND ALSO FREZING THE OBJECT.
 
 JsUser.email = 'asu@chatgpt.com' //this will change the obj email.
 JsUser["full name"] = 'taimur shajak'
@@ -27,8 +42,14 @@ JsUser["full name"] = 'taimur shajak'
 // console.log(JsUser['email']);
 // console.log(JsUser['full name']);
 
+
+
+
+
+//HOW TO ADD FUNCTION IN OBJECT.
+
 JsUser.greeting = function(){
-    console.log('hello is user motherfather');
+    console.log('hello Js user motherfather');
 }
 
 JsUser.greetingTwo = function(){
@@ -49,19 +70,17 @@ JsUser.greetingTwo = function(){
 
 
 
-
 //================PART-2======================
 
 
 
 
 
+//HOW TO DECLARE A OBJECT IN CONSTRUCTOR SINGLETON.
 
-
-//how to declare a object in constructor singleton
 
 //const tinderUser = new Object() // this is a singleton object
-const tinderUser = {} //non single ton object
+const tinderUser = {} //non single ton object ,where there is key and values define after the declaration,like ,in down we write
 
 tinderUser.id = "124abx"
 tinderUser.name = 'binod'
@@ -69,12 +88,21 @@ tinderUser.isLoggedIn = false
 
 //console.log(tinderUser);
 
-// console.log(Object.keys(tinderUser)); //this keywords access the obejcts keys in array form. 
-// console.log(Object.values(tinderUser)); //this keywords access the objects values in array form.
-// console.log(Object.entries(tinderUser)); // this keywords is access the every  keys and values in one array and hold all key values at once array.
-// console.log(tinderUser.hasOwnProperty('isLoggedIn')); //this types od acces also happen
+// console.log(Object.keys(tinderUser)); // if i have to know the keys of an object,then i use this method,bcz of so manny array we need to specify the object name.so it output data types is the array(where i can use loop).
+
+// console.log(Object.values(tinderUser)); //its also same as the upperr one.
+
+// console.log(Object.entries(tinderUser)); // this keywords is access the every  keys and values in one array and hold all key values at another  array.LIKE NASTED ARRAY,where 1st is key the 2nd one is values.(NOT USES THAT MUCH)
+
+// console.log(tinderUser.hasOwnProperty('isLoggedIn')); //if i have to check the keys of the obj is available or not,then we use this method,becouse we are gonna loop through the obj array,if the property we looping is not available THEN the website will be crash probably.
 
 
+
+
+
+
+
+//DECLARE OBJECT INSIDE THE OBJECT(NASTED OBJ) AND ACCESS IT
 
 const regulerUser = {
     email: 'binod@google.com',
@@ -85,25 +113,46 @@ const regulerUser = {
         }
     }
 }
-
 //console.log(regulerUser.full_name.userfullname.lastname);
 
+//console.log(regulerUser.full_name?.userfullname.lastname);
+//many times we dont know the keys is available in the object or not ,if we doubt it existence,then we use a question mark insted of use loop (LIKE if else) to save the time(the process is called optional chaining)
+
+
+
+
+
+
+
+//HOW TO COMBINE OBJECT IN ANOTHER OBJECT.
 
 const obj1 = {1: 'a', 2: 'b'}
 const obj2 = {3: 'c', 4: 'd'}
 const obj3 = {5: 'e', 6: 'f'}
 
-const obj4 = { obj1,  obj2} //this is doing object inside thhe object
+const obj4 = { obj1,  obj2} //this is doing NASTED object LIKE object inside the object
+
 //console.log(obj4);
 
-const obj5 = Object.assign ({}, obj1, obj2, obj3)//to marged on one object 
+
+const obj5 = Object.assign ( obj1, obj2, obj3)
+const obj5 = Object.assign ({}, obj1, obj2, obj3)//to marged the object in one object,and if we use {} empty obj, it actually work like target and marged all as a sources.
+
 //console.log(obj5);
 
-const obj6 = {...obj1, ...obj2, ...obj3} //this doing same things what assign can dom marge in one obj.
+
+
+
+const obj6 = {...obj1, ...obj2, ...obj3} //this is spread operator.this doing same things what assign can do,marge the whole object, and this is the modern way
+
 //console.log(obj6);
 
 
-//=====
+
+
+
+
+//===IF THE OBJECT COME FROM THE DATABSE*(ITS ACTUALLY COME IN ARRAY OF OBJECT LIKE THIS)
 const newUsers = [
     {
         id: 001,
@@ -122,19 +171,61 @@ console.log(newUsers[001].email);//to access the exact data .
 
 
 
-//======
+
+
+
+
+
+//======DE-STRUCTURING OF OBJECT
+
 const mycourse = {
     coursename: 'js hindi by hitesh',
     price: '999',
     courseInstructor: 'hitesh bruh'
 }
-mycourse.courseInstructor
+mycourse.courseInstructor//GENERAL way to accescing the obj values
 
-const {courseInstructor: instructor} = mycourse
+
+const {courseInstructor} = mycourse
+const {courseInstructor: instructor} = mycourse//so this is basically doing when we need to print values 3 4 or more times,then general way to print is unprofessional,instead of this we use this and also we naming the keys is anytingh we want,this is calld de structure
+
+
 //console.log(courseInstructor);
 //console.log(instructor);
 
-/*=======
+
+
+
+
+
+//WHERE DE STRUCTURING APPLY ON (REACTJs)
+
+const navbar = (props.company) => {
+}
+navbar(company = 'abdullah')
+//SO every time we dont write props.company,props.name or props.something else,instead of this we write down below
+
+const navbar = ({company}) => {
+
+}
+navbar(company = 'abdullah')
+
+
+
+
+
+
+
+/*======API concept of OBJ
+
+API Is some values come from the beckend,and how i wrote those values.
+in earlier it comes xml form to complex
+now values comes in JSON form,
+
+so JSON(JS OBJ NOTATION) is look like object,where this doesnt have any name and there keys and values name written in string form.HERE THIS IS
+
+
+
 {
     'name': 'mathmatics'
     'coursename': 'math fundamental'
@@ -142,7 +233,7 @@ const {courseInstructor: instructor} = mycourse
 }
 
 
-//====
+//==SOMETIMES API WE GET AN ARRAY FORMAT
 [
     {},
     {},
@@ -150,6 +241,7 @@ const {courseInstructor: instructor} = mycourse
     {}
 ]
 
+//RANDOMUSERME.COM(GET API) GOOO AND JSON FORMATOR(THAT API DATA STRUCTURE THE FORMAT)
 
 
 
