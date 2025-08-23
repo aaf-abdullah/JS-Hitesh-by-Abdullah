ef03396659dc5a32a912a349cc8fdc6d8d7effcc
Abdullah <mdaaf.abdullah@gmail.com>
2025-08-23 20:05:27 +0000

    learning aout scopes.

---
+++ 02_basics/04_scopes.js
@@ -0,0 +1,34 @@
+//this start with the varible let var and const
+
+let a = 1
+var b = 2
+const c = 3
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+//{} carlwy bracis  this is a scope in every language.if this came in function loop condition then this is scope.but not in objecs
+
+
+
+
+var z = 300
+if (true) {
+    let x = 10
+    const y = 20
+    var z = 30 
+}
+//console.log(x); //error
+//console.log(y);//error
+console.log(z); //executed 
+
+
+
+
+// so here is the scopes is inner the if condition carley bracis {}| outside the curley bracis all are global scopes.
+//so scopes { } are available inside the curley bracis there is o access outside i mean global scopes.
+
+
+
+
+//node 02_basics/04_scopes.js
