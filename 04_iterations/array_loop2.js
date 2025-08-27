57c86f04e0b3a24dabdf059116123e82699210e7
Abdullah <mdaaf.abdullah@gmail.com>
2025-08-27 16:43:49 +0000

    looping in array.

--- 04_iterations/array_loop2.js
+++ 04_iterations/array_loop2.js
@@ -18,6 +18,7 @@ coding.forEach( (val) => {
 function printMe (item){
         //console.log(item);
 } //3rd way
+
 coding.forEach(printMe)//so here is the reference of the function,not the executed (),like this
 
 
@@ -51,8 +52,117 @@ const myCoding = [
 ]
 
 myCoding.forEach(  (item) => {
-        console.log(item.languageFileName);   
+      // console.log(item.languageFileName);   
 })
+
+
+
+
+//==============2nd part================
+
+const coding2 =['js', 'rb', 'py', 'java', 'cpp']
+const value = coding2.forEach( function (item){
+   // console.log(item);
+
+} )
+//console.log(value); //undefined || so basically for each loop doesnt return any value though you use return ,
+
+
+
+
+//FILTER LOOP in array
+
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+const newNum1 = myNums.filter( (num) => num > 4)
+
+const newNum2 = myNums.filter( (num) => {
+    return num > 4
+ })
+
+//console.log(newNum1) //[ 5, 6, 7, 8, 9 ]
+//console.log(newNum2) 
+
+//so filter is array loop like the for each loop,where for each doesnt return the value,but filter do.
+
+
+
+//USING FOR EACH LOOP
+const newNums3 = []
+myNums.forEach( (num) => {
+    if  (  num > 4){
+        newNums3.push(num)
+    }
+} )
+//console.log(newNums3);
+
+
+
+
+//EXAMPLE LOOK LIKE DATABSE DATA
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+let userBooks1 = books.filter ( (bk) => bk.genre === "History" )
+
+userBooks1 = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+
+//console.log(userBooks1)
+
+
+
+
+const userBooks2 = []
+books.forEach( (bk) => {
+        if ( bk.edition > 2000){
+            userBooks2.push(bk)
+        }
+})
+//console.log(userBooks2)
+
+
+
+//========IF CALCULATION NEEDED THAN
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+const newNum4 = myNumbers.filter((num) => {
+   (num + 4)
+    return 
+})
+//console.log(newNum4) //why filter doesnt works here /// BCZ OF map
+
+
+
+//MAP METHOD in array doing also calculation 
+
+const newNum5 = myNumbers.map( num => num + 10)
+//console.log(newNum5);
+
+
+
+//CHAINING IN ARRAY 
+const mNums2 = myNumbers
+        .map( (num) => num * 7)
+        .map( (num) => num + 1 )
+        .filter( (num) => num >= 33)
+    console.log(mNums2);
+    
+
+
 
 
 
