6603085b2792854298f6660ad5a8342dc8474e2a
Abdullah <mdaaf.abdullah@gmail.com>
2025-09-14 01:38:35 +0600

    promise

---
+++ 08_Advanced_JS/02_promise.js
@@ -0,0 +1,14 @@
+//CREATION OF PROMISES
+
+
+const promiseOne = new Promise(function(resolve, rejcet)){
+    
+    //do an async task
+    //DB cals ,, cryptography ,, network calls,, 
+
+   set
+
+} //its like an obj,//PROMISE REDUCE THE CALLBACK HELL
+
+
+
